10a1eca031431b7c8fcd6e9efb0bcd3248bbf020
Checkers-Robot-Project <dara.rattigan.2022@mumail.ie>
2026-05-11 04:08:53 +0100

    Update Square.js

--- src/Square.js
+++ src/Square.js
@@ -3,10 +3,8 @@ import React from "react";
 export default function Square({ value, isDark, label }) {
   return (
     <div className={`cell ${isDark ? "dark" : "light"}`}>
-      {/* Square coordinate label */}
       {isDark && <div className="label">{label}</div>}
 
-      {/* Pieces */}
       {value === 1 && <div className="piece red" />}
       {value === 2 && <div className="piece yellow" />}
       {value === 3 && <div className="piece red king">👑</div>}
